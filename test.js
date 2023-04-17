d5f579a4fc26d9bef2e39ab442cf7bfd52d0c374
Glib Shemenkov <shemenkovglib@gmail.com>
2023-04-18 01:35:26 +0200

    feat:add transform

--- test.js
+++ test.js
@@ -1,6 +1,45 @@
-function getEmailDomain(email) {
-	return email.match(/\@[a-z].+\.com/g).join('').slice(1)
-}
+function transform(arr) {
+	if (!Array.isArray(arr)) {
+	  throw new Error('arr parameter must be an instance of the Array!');
+	}
 
-console.log(getEmailDomain('example-indeed@strange-example.com'))
+	const result = [];
+
+	for (let i = 0; i < arr.length; i++) {
+	  const currentElement = arr[i];
+
+
+		 if(currentElement ==='--discard-next'){
+			i++;
+		 }
+
+		else if (currentElement === '--discard-prev'){
+			if (i > 0 && arr[i - 2] !== '--discard-next') {
+			  result.pop();
+			}
+		}
+
+		else if (currentElement === '--double-next'){
+			if (i < arr.length - 1) {
+			  result.push(arr[i + 1]);
+			}
+		}
+
+		else if (currentElement === '--double-prev') {
+			if (i > 0 && arr[i - 2] !== '--discard-next') {
+			  result.push(arr[i - 1]);
+			}
+		}
+
+		  else {
+		//   if (i < arr.length && arr[i + 1] !== '--discard-prev') {
+			  result.push(currentElement);
+
+	  }
+	}
+
+	return result;
+ }
+
+console.log(transform([1, 2, 3, '--double-next', 1337, '--discard-prev', 4, 5]))
 //  usual.com))
